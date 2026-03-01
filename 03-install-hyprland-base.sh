b680e8f8dc223d1c23ab0193eed8212c5e3bf715
Thalles <thalles.guimaraes456@gmail.com>
2026-03-01 19:50:16 -0300

    update script

--- 03-install-hyprland-base.sh
+++ 03-install-hyprland-base.sh
@@ -11,5 +11,5 @@ echo "========================================="
 # 6. INSTALANDO HYPRLAND (E ECOSSISTEMA)
 # ---------------------------------------------------
 echo "[6/6] Instalando Hyprland..."
-sudo pacman -S --needed --noconfirm hyprland kitty firefox waybar #rofi-wayland dunst xdg-desktop-portal-hyprland polkit-kde-agent
+sudo pacman -S --needed --noconfirm hyprland kitty firefox waybar hyprpolkitagent hyprpaper dunst hyprlauncher nemo xdg-desktop-portal-hyprland wl-copy #rofi-wayland dunst xdg-desktop-portal-hyprland polkit-kde-agent
 
